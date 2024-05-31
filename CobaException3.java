0be33a75a645d1aabaec70e264c6fabc71a79a56
AnnisaTasyaAulia <114630085+AnnisaTasyaAulia@users.noreply.github.com>
2024-05-31 18:52:07 +0700

    Add files via upload

---
+++ CobaException3.java
@@ -0,0 +1,19 @@
+
+package Tugas11;
+
+
+public class CobaException3 {
+    
+    public static void main(String[] args) {
+        int bil=10;
+        try
+        {
+            System.out.println("bil/o");
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Terjadi Aritmatika error");
+        }
+    }
+    
+}
